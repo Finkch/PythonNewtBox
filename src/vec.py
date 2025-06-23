bc672952fa97b9cc2c655d768ea429b9220d9b0b
Finkch <skyler@thealdersons.org>
2025-06-23 12:21:46 -0700

    Added basic arithmetic operators to Vectors

--- src/vec.py
+++ src/vec.py
@@ -1,9 +1,52 @@
 # A Vec is a 3-component vector
 
+from __future__ import annotations
 from decimal import Decimal
 
-class Vec:
+class Vector:
     def __init__(self, x: Decimal | float = 0, y: Decimal | float = 0, z: Decimal | float = 0):
         self.x: Decimal = Decimal(x)
         self.y: Decimal = Decimal(y)
         self.z: Decimal = Decimal(z)
+    
+    
+    # Dunder methods for arithmetic operators
+    def __add__(self, other: object) -> Vector:
+        if not isinstance(other, Vector):
+            raise ArithmeticError(f'Cannot add Vector to type {type(other)}')
+        
+        return Vector(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z,
+        )
+        
+    def __sub__(self, other: object) -> Vector:
+        if not isinstance(other, Vector):
+            raise ArithmeticError(f'Cannot subtract Vector by type {type(other)}')
+        
+        return Vector(
+            self.x - other.x,
+            self.y - other.y,
+            self.z - other.z,
+        )
+        
+    def __mul__(self, other: object) -> Vector:
+        if not isinstance(other, (int, float, Decimal)):
+            raise ArithmeticError(f'Cannot multiply Vector by non-scalar of type {type(other)}')
+        
+        return Vector(
+            self.x * Decimal(other),
+            self.y * Decimal(other),
+            self.z * Decimal(other),
+        )
+    
+    def __div__(self, other: object) -> Vector:
+        if not isinstance(other, (int, float, Decimal)):
+            raise ArithmeticError(f'Cannot divide Vector by non-scalar of type {type(other)}')
+            
+        return Vector(
+            self.x / Decimal(other),
+            self.y / Decimal(other),
+            self.z / Decimal(other),
+        )
