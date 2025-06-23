fb7fd23282bbe221938bd4287945616b06861687
Finkch <skyler@thealdersons.org>
2025-06-23 16:18:14 -0700

    Added logging

--- main.py
+++ main.py
@@ -3,11 +3,20 @@
 # This project will use the default 28-digits of decimal precision.
 from decimal import Decimal
 
+from src.logging import logs_setup
+import logging
+
 from src.body import Body
 from src.vector import Vector
 from src.simulate import simulate
 
+logger = logging.getLogger(__name__)
+
+
 if __name__ == '__main__':
+    
+    # Sets up the logger
+    logs_setup(file_level = logging.INFO, stream_level = logging.DEBUG)
     
     sol: Body   = Body('Sol', 1.989e30)
     terra: Body = Body(
@@ -16,5 +25,6 @@ if __name__ == '__main__':
         pos = Vector(x = 1.496e11),
         vel = Vector(y = 2.978e4),
     )
+    bodies = [sol, terra]
     
-    simulate(Decimal(1 * 60), [sol, terra])
+    simulate(Decimal(1 * 60), bodies)
